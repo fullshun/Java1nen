2e3a493daae6192868b1919416564a31523ad92b
Shuntaro_Furutaka <fishermansbaster@gmail.com>
2025-03-21 14:55:52 +0900

    新規ファイル追加1(13,14)

--- Main_chap2_13.java
+++ Main_chap2_13.java
@@ -1,10 +1,10 @@
 public class Main_chap2_13 {
     public static void main(String[] args) {
         int a = 10 ;
-        a *= 2 ;
-        a += 2;
-        a -= 2;
-        a /= 2;
+        a *= 2 ;    // 10 * 2 = 20
+        a += 2;     // 20 + 2 = 22
+        a -= 2;     // 22 - 2 = 20
+        a /= 2;    // 20 / 2 = 10
         System.out.println(a);
     }
 }
---
+++ Main_chap2_14.java
@@ -0,0 +1,9 @@
+public class Main_chap2_14 {
+    public static void main(String[] args) {
+        int a = 10;
+        a--;    // 10 - 1 = 9
+        a++;    // 9 + 1 = 10
+        a++;    // 10 + 1 = 11
+        System.out.println(a); 
+    }
+}
